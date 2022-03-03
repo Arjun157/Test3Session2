d1c01c65465c3be3ba96c0680af01c8d93708ff2
Arjun G <97089698+Arjun157@users.noreply.github.com>
2022-03-03 08:55:59 +0000

    P7

--- p7final.c
+++ p7final.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+struct_fraction
+{
+  int num, den;
+}
+typedeft_fraction fraction;
+int gcb(int a, intb)
+{
+  int t;
+  while(b! =0)
+  {
+    t=b:
+    b=a%b;
+    a=t;
+  }
+  return a;
+  }
+fraction input_fraction() 
+{
+  fraction f;
+  printf("input a fraction\n");
+  scanf("%d%d",&f,num,&f.den);
+}
+int main()
+{
+  printf("%d\n",gcd(16,24));
+}
