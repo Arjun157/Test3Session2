1ec6953c61d648e985a18072e167c0654316baad
Arjun G <97089698+Arjun157@users.noreply.github.com>
2022-03-09 04:00:17 +0000

    pp

--- p2final.c
+++ p2final.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
-struct _fraction{
+struct _fraction
+{
   float num, den;
 };
 
 typedef struct _fraction fraction;
 
-fraction input_fraction(){
+fraction input_fraction()
+{
   fraction f;
   printf("enter the value of fraction to get smallest\n");
   scanf("%f %f", &f.num, &f.den);
@@ -14,27 +16,32 @@ fraction input_fraction(){
   return f;
 }
 
-fraction smallest_fraction(fraction f1, fraction f2, fraction f3){
+fraction smallest_fraction(fraction f1, fraction f2, fraction f3)
+{
   float a, b, c;
   a = f1.num/f2.den;
   b = f2.num/f2.den;
   c = f3.num/f3.den;
 
-  if(a<b && a<c){
+  if(a<b && a<c)
+  {
     return f1;
     }
-  else if(b<a && b<c){
+  else if(b<a && b<c)
+  {
     return f2;
     }
   else
     return f3;
 }
 
-void output(fraction f1, fraction f2, fraction f3, fraction smallest){
+void output(fraction f1, fraction f2, fraction f3, fraction smallest)
+{
   printf("smalest fraction among %.0f/%.0f, %.0f/%.0f and %.0f/%.0f is %.0f/%.0f\n", f1.num, f1.den, f2.num, f2.den, f3.num, f3.den, smallest.num, smallest.den);
 }
 
-int main(){
+int main()
+{
   fraction f1, f2, f3, smallest;
   f1 = input_fraction();
   f2 = input_fraction();
