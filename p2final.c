5ef7e88251aa3186aefd2d16f7a5cb4e4ef78331
Arjun G <97089698+Arjun157@users.noreply.github.com>
2022-03-09 03:52:37 +0000

    P..

--- p2final.c
+++ p2final.c
@@ -1,1 +1,17 @@
-
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction
+Fraction input_fraction()
+{
+  fraction f;
+  printf("enter a fraction\n");
+  scan("%d",&f);
+  return f;
+}
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+  if(f1<f2&&f1<f3)
+}
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
--- p8final.c
+++ p8final.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct _fraction Fraction;
 
+  int gcd(int a,int b)
+{
+  int t;
+  while (b!=0)
+    {
+      t = b;
+      b = a%b;
+      a = t;
+    }
+  return a;
+int input()
+{
+  print("enter a fraction\n")
+}
+}
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+void input_n_fraction(int n, fraction f[n])
+{
+  for(int i=0;;)action add_fraction(Fraction f1,Fraction f2)
+{
+  Fraction sum;
+  sum.num = f1.num*f2.den+f2.num*f1.den;
+  sum.den = f1.den*f2.den;
+  int g = gcd(sum.num,sum.den);
+  sum.num=sum.num/g;
+  sum.den=sum.den/g;
+  return sum;
+}
+int main()
+{
+  Fraction f1,f2,sum;
+  f1 = input_fraction();
+  f2 = input_fraction();
+  sum = add_fraction(f1,f2);
+  printf("%d/%d + %d/%d = %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
+  return 0;
+}
